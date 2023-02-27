daf5eab828960d74ba594602b69c73b1c6533883
b2 <brian@bsquared-tech.com>
2023-02-27 12:54:07 -0600

    test all lookup

--- ai-test/app.rb
+++ ai-test/app.rb
@@ -2,13 +2,57 @@ require 'sinatra'
 require 'resolv'
 require 'json'
 require 'byebug'
+require 'net/http'
 get '/' do
   erb :index
 end
 
 post '/lookup' do
   host = params[:host]
-  type = params[:type].upcase
+  types = ["NS", "A", "AAAA", "CNAME", "MX", "TXT"]
+  all_records = {}
+  types.each do |t|
+    records = Resolv::DNS.new.getresources(host, Resolv::DNS::Resource::IN.const_get(t))
+    record_data = ""
+    begin
+      result = records.map do |r|
+        case t 
+        when "CNAME"
+          record_data = r.name.to_s
+        when "A", "AAAA" 
+          record_data = r.address.to_s
+        when "NS"
+          record_data = r.name.to_s
+        when "MX"
+          record_data = "#{r.preference} #{r.exchange.to_s}"
+        when "TXT"
+          record_data = r.data.to_s
+        else
+          record_data = r.data.to_s
+        end
+      end
+      all_records[t] = record_data     
+    rescue Resolv::ResolvError => e
+      content_type :json
+      { status: 'error', message: e.message }.to_json
+    end
+  end
+
+  uri = URI::HTTP.build(host: host)
+  response = Net::HTTP.get_response(uri)
+  all_records["HTTP Response code"] = response.code
+ 
+  uri = URI::HTTP.build(host: host, scheme: "https")
+  response = Net::HTTP.get_response(uri)
+  all_records["HTTPS Response code"] = response.code
+
+  all_records["Your IP"] = request.env["REMOTE_ADDR"]
+byebug
+  content_type :json
+  { status: 'success', result: all_records }.to_json
+=begin
+#  type = params[:type].upcase
+
   records = Resolv::DNS.new.getresources(host, Resolv::DNS::Resource::IN.const_get(type))
   begin
     result = records.map do |r|
@@ -34,4 +78,5 @@ post '/lookup' do
     content_type :json
     { status: 'error', message: e.message }.to_json
   end
+=end
 end
